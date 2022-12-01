28b64fe411db44e183c0fb941de489ab711c553e
joao vitor <guerreiroxx8@gmail.com>
2022-12-01 14:44:21 -0300

    iniciado

---
+++ Nubank/Entities/Entities/Notifies.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notitycoes= new List<Notifies>();
+        }
+
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+        [NotMapped]
+        public string Mensagem { get; set; }
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(nomePropriedade) || string.IsNullOrWhiteSpace(valor))
+            {
+                Notitycoes.Add(new Notifies{
+
+                    Mensagem = "Campo obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+                return false;
+
+            }
+            return true;
+
+        }
+    }
+}
