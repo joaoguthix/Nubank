30f947f2d936e41ad55e17baccfe3019a0160718
joao vitor <guerreiroxx8@gmail.com>
2022-12-02 01:34:44 -0300

    iniciando o ContextBase

---
+++ Nubank/Infrastucture/Configuration/ContextBase.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        { 
+
+        }
+            public DbSet<DebitCard> DebitCards { get; set; }
+            public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+
+        
+        
+    }
+}
